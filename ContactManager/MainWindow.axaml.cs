258492e640e7ebf8a4caf888d3bbf2276d5b9d96
woojtylaGG <wojciech.piskorski@cyfroweszkoly.pl>
2024-11-14 19:51:31 +0100

    Addbutton i Filtr

--- ContactManager/MainWindow.axaml.cs
+++ ContactManager/MainWindow.axaml.cs
@@ -1,11 +1,37 @@
 using Avalonia.Controls;
+using System.Collections.ObjectModel;
 
 namespace ContactManager;
-
-public partial class MainWindow : Window
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private ObservableCollection<Contact> Contacts { get; } = new ObservableCollection<Contact>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContactsListBox.ItemsSource = Contacts;
+            FilterComboBox.SelectedIndex = 0;
+        }
+
+        private void OnAddEditButtonClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var contact = new Contact
+            {
+                FirstName = FirstNameTextBox.Text,
+                LastName = LastNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text,
+                IsFavorite = FavoriteCheckBox.IsChecked ?? false
+            };
+
+            Contacts.Add(contact);
+            ClearInputFields();
+            ApplyFilterAndSort();
+        }
+        private void OnFilterChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplyFilterAndSort();
+        }
     }
 }
